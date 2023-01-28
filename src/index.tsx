642f3a24adc8fe61a8b6c58b19dad1d6751d4bc5
hongsoom <hongsoom@kakao.com>
2023-01-29 04:09:03 +0900

    [Feat] 필요한 페이지 경로 지정

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Videos from './pages/Videos';
+import VideoDetail from './pages/VideoDetail';
+import Subscribe from './pages/Subscribe';
+import Finder from './pages/Finder';
+import History from './pages/History';
+import Storage from './pages/Storage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: 'videos', element: <Videos /> },
+      { path: 'videos/:keyword', element: <Videos /> },
+      { path: 'videos/watch/:videoId', element: <VideoDetail /> },
+      { path: 'subscribe', element: <Subscribe /> },
+      { path: 'finder', element: <Finder /> },
+      { path: 'history', element: <History /> },
+      { path: 'storage', element: <Storage /> }
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
