6fccea68785d9e79d15c679feccd17b210063f9d
hongsoom <hongsoom@kakao.com>
2023-02-02 22:47:35 +0900

    [Design] 컴포넌트 화면 다시 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Outlet } from 'react-router';
 import { YoutubeAPIProvider } from './contenxt/YoutubeAPIContext';
 import NavBar from "./components/navBar/NavBar";
-import Sidebar from './components/navBar/SideBar';
 
 const queryclient = new QueryClient();
 
@@ -12,22 +10,13 @@ const App = () => {
   return (
     <>
       <NavBar />
-      <Body>
-        <Sidebar />
-        <QueryClientProvider client={queryclient} >
-          <YoutubeAPIProvider>
-            <Outlet />
-          </YoutubeAPIProvider>
-        </QueryClientProvider>
-      </Body>
+      <QueryClientProvider client={queryclient} >
+        <YoutubeAPIProvider>
+          <Outlet />
+        </YoutubeAPIProvider>
+      </QueryClientProvider>
     </>
   );
 }
 
 export default App;
-
-const Body = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-`;
