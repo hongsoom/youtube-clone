fed26bb567c536b72b3ec0c5c699c2c79dfe3e27
hongsoom <hongsoom@kakao.com>
2023-02-06 17:08:49 +0900

    [Feature] 항상 있어야할 sideBar APP.tsx에 선언 후 필요없을 시 조건 걸어서  보이지 않게 함

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
+import styled from 'styled-components';
 import { YoutubeAPIProvider } from './contenxt/YoutubeAPIContext';
 import NavBar from "./components/navBar/NavBar";
+import Sidebar from './components/navBar/SideBar';
 
 const queryclient = new QueryClient();
 
 const App = () => {
+  const location = useLocation();
+  const pathname = location.pathname
+
   return (
     <>
       <NavBar />
-      <QueryClientProvider client={queryclient} >
-        <YoutubeAPIProvider>
-          <Outlet />
-        </YoutubeAPIProvider>
-      </QueryClientProvider>
+      <VideoMain pathname={pathname}>
+        <QueryClientProvider client={queryclient} >
+          <YoutubeAPIProvider>
+            {pathname.includes('watch') === false &&
+              <Sidebar />}
+            <Outlet />
+          </YoutubeAPIProvider>
+        </QueryClientProvider>
+      </VideoMain>
     </>
   );
 }
 
 export default App;
+
+const VideoMain = styled.div< { pathname: string }>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  @media all and (max-width: 1024px) {
+    flex-wrap : ${(props) => (props.pathname === '/' && 'wrap')};
+  }
+`;
