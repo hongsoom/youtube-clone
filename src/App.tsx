2e5aab2f37a1ae9d2f67de67f6265382c6219949
hongsoom <hongsoom@kakao.com>
2023-03-02 23:24:06 +0900

    [Feat] App.js에는 길게 쓰지 않고 필요한 도구들만 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
-import React, { useState, memo } from 'react';
+import React, { memo } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { Outlet, useLocation } from 'react-router';
-import styled from 'styled-components';
+import { Outlet } from 'react-router';
 import { YoutubeAPIProvider } from './contenxt/YoutubeAPIContext';
-import { RouteState } from './types/types';
-import NavBar from "./components/navBar/NavBar";
-import Sidebar from './components/navBar/SideBar';
 
 const queryclient = new QueryClient();
 
 const App = () => {
-  const [open, setOpen] = useState<boolean>(false);
-  const location = useLocation() as RouteState;
-  const pathname: string = location.pathname
-
   return (
-    <>
-      <NavBar setOpen={setOpen} open={open} />
-      <VideoMain pathname={pathname}>
-        <QueryClientProvider client={queryclient} >
-          <YoutubeAPIProvider>
-            {pathname.includes('watch') === false &&
-              <Sidebar open={open} />}
-            <Outlet />
-          </YoutubeAPIProvider>
-        </QueryClientProvider>
-      </VideoMain>
-    </>
+    <QueryClientProvider client={queryclient} >
+      <YoutubeAPIProvider>
+        <Outlet />
+      </YoutubeAPIProvider>
+    </QueryClientProvider>
   );
 }
 
 export default memo(App);
-
-const VideoMain = styled.div< { pathname: string }>`
-  width: 100%;
-  height: 100%;
-  display: flex;
-
-  @media all and (max-width: 1024px) {
-    flex-wrap : ${(props) => (props.pathname.includes('watch') && 'wrap')};
-  }
-`;
