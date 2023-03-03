3e6957727cf4901313a4969e29224605273ac5b1
hongsoom <hongsoom@kakao.com>
2023-03-03 15:34:15 +0900

    [Feat] 레이아웃이 똑같은 페이지들 각자 선언해서 쓰지 않고 하나의 컴포넌트로 폼을 만들어 가져와 사용

---
+++ src/components/PageForm.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { ButtonWrap } from '../style/Layout';
+import { LoginBtn, Profile } from '../style/Buuton';
+import { GOOGLE_AUTH_URL } from "../shared/OAuth";
+import { FormType } from "../types/types";
+
+const PageForm = ({
+    icon,
+    login,
+    notlogin,
+    notloginD }: FormType) => {
+
+    const hash = sessionStorage.getItem("hash");
+
+    return (
+        <ButtonWrap>
+            {hash ?
+                <h1>{login}</h1>
+                :
+                <>
+                    <h1>{notlogin}</h1>
+                    <p>{notloginD}</p>
+                    <LoginBtn onClick={GOOGLE_AUTH_URL}>
+                        <Profile />로그인
+                    </LoginBtn>
+                </>}
+        </ButtonWrap>
+    )
+}
+
+export default PageForm;
+
+
