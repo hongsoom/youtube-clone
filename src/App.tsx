cea9dbb6000f98751a0ae2156d2035aa17a1f3b6
hongsoom <hongsoom@kakao.com>
2023-03-09 13:08:42 +0900

    [Feat] 항상 페이지에 고정으로 나와야 하는 컴포넌트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router';
 import { Helmet } from 'react-helmet-async';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Outlet } from 'react-router';
+import styled from 'styled-components';
+import NavBar from './components/NavBar/NavBar';
+import SideBar from './components/SideBar/SideBar';
 import { YoutubeAPIProvider } from './contenxt/YoutubeAPIContext';
+import { RouteState } from './types/types';
 
 const queryclient = new QueryClient();
 
 const App = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const location = useLocation() as RouteState;
+  const pathname: string = location.pathname;
   return (
     <>
       <Helmet>
-        <title>Youtube</title>
+        <title>Youtube TEST</title>
       </Helmet>
-      <QueryClientProvider client={queryclient} >
+      <QueryClientProvider client={queryclient}>
         <YoutubeAPIProvider>
-          <Outlet />
+          <NavBar setOpen={setOpen} open={open} />
+          <VideoMain pathname={pathname}>
+            <SideBar open={open} />
+            <Outlet />
+          </VideoMain>
         </YoutubeAPIProvider>
       </QueryClientProvider>
     </>
   );
-}
+};
 
 export default App;
+
+const VideoMain = styled.div<{ pathname: string }>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  @media all and (max-width: 1024px) {
+    flex-wrap: ${props => props.pathname.includes('watch') && 'wrap'};
+  }
+`;
